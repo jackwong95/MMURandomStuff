a064269712e21c1be72af2c591fb5fdcb9fd8da4
Jack <jackwongtiongkiat@gmail.com>
2016-01-05 23:00:48 +0800

    Super Solid Linked List Update

--- TCP1201 - OOPDS/LinkedList/include/LinkedList.h
+++ TCP1201 - OOPDS/LinkedList/include/LinkedList.h
@@ -2,7 +2,8 @@
 #define LINKEDLIST_H
 
 #include <iostream>
-using namespace std;
+#include <stdexcept>
+#include <exception>
 
 template <typename T>
 struct Node
@@ -11,12 +12,27 @@ struct Node
     Node *next = nullptr;
 };
 
+///functions available:
+/*
+void clr();
+void reserve(int size);
+void insert_at(int idx, T item)
+void push_head(T item)
+void push_tail(T item)
+void pop_head(T item)
+void pop_tail(T item)
+void display()
+void removeAt(int _idx)
+T& getAt(int i)
+int findIdx(T item)
+int sz()
+*/
 template <class T>
 class LinkedList
 {
     public:
-        LinkedList(int _N = 0)
-        :N(_N)
+        LinkedList()
+        :N(0)
         {
         }
 
@@ -24,11 +40,42 @@ class LinkedList
         {
         }
 
+        void clr()
+        {
+            while(headNode!=nullptr&&tailNode!=nullptr)
+            {
+                pop_head();
+            }
+        }
+
+        void reserve_from_tail(int sz)
+        {
+            for(int i = 0; i < sz; i ++)
+            {
+                Node<T> newNode;
+                push_tail(newNode.item);
+            }
+        }
+
+        void reserve_from_head(int sz)
+        {
+            for(int i = 0; i < sz; i ++)
+            {
+                Node<T> newNode;
+                push_head(newNode.item);
+            }
+        }
+
+        void insert_at(int idx, T item)
+        {
+            getNode(idx) = item;
+        }
+
         void push_tail(T item)
         {
             Node<T>* newNode = new Node<T>;
             newNode->item = item;
-            if(tailNode == nullptr)
+            if(N==0)
             {
                 tailNode = newNode;
                 headNode = newNode;
@@ -41,13 +88,58 @@ class LinkedList
             N++;
         }
 
+        void push_head(T item)
+        {
+            Node<T>* newNode = new Node<T>;
+            newNode->item = item;
+            if(N==0)
+            {
+                tailNode = newNode;
+                headNode = newNode;
+            }
+            else
+            {
+                newNode->next = headNode;
+                headNode = newNode;
+            }
+            N++;
+        }
+        T pop_head()
+        {
+            if(headNode==nullptr)
+                throw std::out_of_range("Attempt to get nullptr node, LinkedList pop_head function.");
+            T item = headNode->item;
+            Node<T>* toDel = headNode;
+            headNode = headNode->next;
+            N--;
+            delete toDel;
+            toDel = nullptr;
+            return item;
+        }
+
+        T pop_tail()
+        {
+            Node<T>* tempNode = getNode(N-2);
+            if(tempNode==nullptr)
+                throw std::out_of_range("Attempt to get nullptr node, LinkedList pop_tail function.");
+            tempNode->next = nullptr;
+            T item = tailNode->item;
+            N--;
+            delete tailNode;
+            tailNode = nullptr;
+            tailNode = tempNode;
+            return item;
+        }
         void display()
         {
+            std::cout<<"Displaying all items in the list"<<std::endl;
+            int idx = 0;
             Node<T>* currNode = headNode;
             while(currNode != nullptr)
             {
-                cout << currNode->item << endl;
+                std::cout << "Item " << idx << " : "  << currNode->item << std::endl;
                 currNode = currNode->next;
+                idx ++;
             }
         }
 
@@ -61,16 +153,30 @@ class LinkedList
                 {
                     if(_idx==0)
                     {
+                        /*
                         headNode = headNode->next;
                         N--;
-                        break;
+                        */
+                        pop_head();
+                        return;
+                    }
+                    else if(_idx==(N-1))
+                    {
+                        /*
+                        delete tailNode;
+                        tailNode = getNode(N-2);
+                        tailNode->next = nullptr;
+                        N--;
+                        */
+                        pop_tail();
+                        return;
                     }
                     else if(idx == _idx && (idx-1)>=0)
                     {
                         Node<T>* previousNode = getNode(idx-1);
                         previousNode->next = currNode->next;
                         N--;
-                        break;
+                        return;
                     }
                     idx ++;
                     currNode = currNode->next;
@@ -86,32 +192,14 @@ class LinkedList
                 Node<T>* currNode = headNode;
                 while(currNode != nullptr)
                 {
-                    if(idx == i) return &(currNode->item);
+                    if(idx == i) return currNode->item;
                     idx ++;
                     currNode = currNode->next;
                 }
             }
-            return nullptr;
+            throw std::out_of_range("Argument out of bounds, LinkedList getAt function.");
         }
-
-        void setAt(int i, T item)
-        {
-            if(i >= 0 && i <= (N-1))
-            {
-                int idx = 0;
-                Node<T>* currNode = headNode;
-                while(currNode != nullptr)
-                {
-                    if(idx == i)
-                    {
-                        currNode.item = item;
-                        idx ++;
-                        currNode = currNode->next;
-                    }
-                }
-            }
-        }
-
+        ///THIS WILL ONLY RETURN THE VERY FIRST VALUE FROM HEAD THAT IS EQUIVILENT OF ITEM
         int findIdx(T item)
         {
             int idx = 0;
@@ -133,7 +221,6 @@ class LinkedList
         Node<T> *headNode = nullptr;
         Node<T> *tailNode = nullptr;
         int N;
-
         Node<T>* getNode(int i)
         {
             if(i >= 0 && i <= (N-1))
@@ -147,7 +234,7 @@ class LinkedList
                     currNode = currNode->next;
                 }
             }
-            return nullptr;
+            throw std::out_of_range("Argument out of bounds, LinkedList getNode function.");
         }
 };
 
--- TCP1201 - OOPDS/LinkedList/main.cpp
+++ TCP1201 - OOPDS/LinkedList/main.cpp
@@ -5,16 +5,32 @@ using namespace std;
 
 int main()
 {
-    LinkedList<int> linkedListed;
-/*
-    linkedListed.push_back(12);
-    linkedListed.push_back(3);
-    linkedListed.push_back(37);
-    linkedListed.push_back(7);
-    linkedListed.remove(0);
-    linkedListed.remove(2);
-*/
-    cout << linkedListed.size() << endl;
-    linkedListed.display();
+    LinkedList<int> ll;
+    ll.push_head(1);
+    ll.push_tail(2);
+    ll.push_head(0);
+    ll.push_tail(3);
+    cout<<"Popped head : "<<ll.pop_head()<<endl;
+    ll.push_head(0);
+    ll.removeAt(0);
+    ll.push_head(0);
+    ll.push_tail(4);
+    ll.removeAt(ll.sz()-1);
+    ll.push_tail(4);
+    cout<<"Popped tail : "<<ll.pop_tail()<<endl;
+    ll.push_tail(4);
+    ll.display();
+    ll.clr();
+    cout<<"Cleared list"<<endl;
+    ll.display();
+    cout<<"Reserved 10 extra spaces from tail"<<endl;
+    ll.reserve_from_tail(10);
+    ll.display();
+    ll.clr();
+    cout<<"Cleared list"<<endl;
+    ll.display();
+    ll.reserve_from_head(3);
+    ll.display();
+    ll.clr();
     return 0;
 }
